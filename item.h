00bb013375f48eb64b53115c525ab32c961f2c40
701004123-cmd <701004123@gotvdsb.ca>
2026-06-10 20:36:49 +0000

    update main.cpp

---
+++ item.h
@@ -0,0 +1,188 @@
+#include <iostream>
+
+using namespace std;
+
+struct oreInfo{
+    string name;
+    int durability;
+    int exp;
+};
+
+struct refinInfo{
+    string name;
+    int exp;
+};
+
+struct toolInfo{
+    string name;
+    int speed;
+};
+
+struct armorInfo{
+    string name;
+    int defense;
+};
+
+class geders{
+    public:
+        vector<oreInfo> ore = {
+            {"oreT1", 1, 10},
+            {"oreT2", 2, 50},
+            {"oreT3", 3, 100},
+            {"oreT4", 4, 500},
+            {"oreT5", 5, 1000},
+            {"oreT6", 6, 5000},
+            {"oreT7", 7, 10000},
+            {"oreT8", 8, 50000000}
+        };
+        vector<oreInfo> wood = {
+            {"woodT1", 1, 10},
+            {"woodT2", 2, 50},
+            {"woodT3", 3, 100},
+            {"woodT4", 4, 500},
+            {"woodT5", 5, 1000},
+            {"woodT6", 6, 5000},
+            {"woodT7", 7, 10000},
+            {"woodT8", 8, 50000000}
+        };
+        vector<oreInfo> leather = {
+            {"leatherT1", 1, 10},
+            {"leatherT2", 2, 50},
+            {"leatherT3", 3, 100},
+            {"leatherT4", 4, 500},
+            {"leatherT5", 5, 1000},
+            {"leatherT6", 6, 5000},
+            {"leatherT7", 7, 10000},
+            {"leatherT8", 8, 50000000}
+        };
+        vector<oreInfo> wool = {
+            {"woolT1", 1, 10},
+            {"woolT2", 2, 50},
+            {"woolT3", 3, 100},
+            {"woolT4", 4, 500},
+            {"woolT5", 5, 1000},
+            {"woolT6", 6, 5000},
+            {"woolT7", 7, 10000},
+            {"woolT8", 8, 50000000}
+        };
+};
+
+class refins{
+    public:
+        vector<refinInfo> bar = {
+            {"barT1", 10},
+            {"barT2", 50},
+            {"barT3", 100},
+            {"barT4", 500},
+            {"barT5", 1000},
+            {"barT6", 5000},
+            {"barT7", 10000},
+            {"barT8", 50000000}
+        };
+        vector<refinInfo> plank = {
+            {"plankT1", 10},
+            {"plankT2", 50},
+            {"plankT3", 100},
+            {"plankT4", 500},
+            {"plankT5", 1000},
+            {"plankT6", 5000},
+            {"plankT7", 10000},
+            {"plankT8", 50000000}
+        };
+        vector<refinInfo> refinedLeather = {
+            {"refinedLeatherT1", 10},
+            {"refinedLeatherT2", 50},
+            {"refinedLeatherT3", 100},
+            {"refinedLeatherT4", 500},
+            {"refinedLeatherT5", 1000},
+            {"refinedLeatherT6", 5000},
+            {"refinedLeatherT7", 10000},
+            {"refinedLeatherT8", 50000000}
+        };
+        vector<refinInfo> fabric = {
+            {"fabricT1", 10},
+            {"fabricT2", 50},
+            {"fabricT3", 100},
+            {"fabricT4", 500},
+            {"fabricT5", 1000},
+            {"fabricT6", 5000},
+            {"fabricT7", 10000},
+            {"fabricT8", 50000000}
+        };
+
+        int durability = 1;
+};
+
+class crafts{
+    public:
+        vector<toolInfo> axe = {
+            {"axeT1", 2},
+            {"axeT2", 4},
+            {"axeT3", 6},
+            {"axeT4", 8},
+            {"axeT5", 10},
+            {"axeT6", 12},
+            {"axeT7", 14}, 
+            {"axeT8", 16}
+        };
+        vector<toolInfo> pick = {
+            {"pickT1", 2}, 
+            {"pickT2", 4}, 
+            {"pickT3", 6}, 
+            {"pickT4", 8},
+            {"pickT5", 10}, 
+            {"pickT6", 12}, 
+            {"pickT7", 14}, 
+            {"pickT8", 16}
+        };
+        vector<toolInfo> sword = {
+            {"swordT1", 10}, 
+            {"swordT2", 50}, 
+            {"swordT3", 100}, 
+            {"swordT4", 500},
+            {"swordT5", 1000}, 
+            {"swordT6", 5000}, 
+            {"swordT7", 10000}, 
+            {"swordT8", 50000}
+        };
+        vector<armorInfo> helmet = {
+            {"helmetT1", 10}, 
+            {"helmetT2", 50}, 
+            {"helmetT3", 100}, 
+            {"helmetT4", 500},
+            {"helmetT5", 1000}, 
+            {"helmetT6", 5000}, 
+            {"helmetT7", 10000}, 
+            {"helmetT8", 50000}
+        };
+        vector<armorInfo> chestplate = {
+            {"chestplateT1", 10}, 
+            {"chestplateT2", 50}, 
+            {"chestplateT3", 100}, 
+            {"chestplateT4", 500},
+            {"chestplateT5", 1000}, 
+            {"chestplateT6", 5000}, 
+            {"chestplateT7", 10000}, 
+            {"chestplateT8", 50000}
+        };
+        vector<armorInfo> leggings = {
+            {"leggingsT1", 10}, 
+            {"leggingsT2", 50}, 
+            {"leggingsT3", 100}, 
+            {"leggingsT4", 500},
+            {"leggingsT5", 1000},
+             {"leggingsT6", 5000}, 
+             {"leggingsT7", 10000}, 
+             {"leggingsT8", 50000}
+        };
+        vector<armorInfo> boots = {
+            {"bootsT1", 10}, 
+            {"bootsT2", 50}, 
+            {"bootsT3", 100}, 
+            {"bootsT4", 500},
+            {"bootsT5", 1000}, 
+            {"bootsT6", 5000}, 
+            {"bootsT7", 10000}, 
+            {"bootsT8", 50000}
+        };
+};
--- main.cpp
+++ main.cpp
@@ -1,18 +1,171 @@
+#include <raylib.h>
 #include <iostream>
 #include <ctime>
-
+#include <cmath>
+#include "character.h"
+#include "level.h"
+#include "item.h"
 using namespace std;
+// global objects
+geders geders1;
+refins refins1;
+crafts crafts1;
+gedering gedering1;
+refining refining1;
+crafting crafting1;
+fighting fighting1;
+inventory inventory1;
 
-void timer(bool starter){
-    int seond
+// global variables
+int gatheringTier;
+int refiningTier;
+int whatCraft;
+int craftingTier;
+int fightingTier;
+int timeEnd;
 
-    while (true) {
-        int s  = saniye / 3600;
-        int dk = (saniye % 3600) / 60;
-        int sn = saniye % 60;
+void addToInventory(string name, int amount){
+    for(int i = 0; i < inventory1.items.size(); i++){
+        if(inventory1.items[i].name == name){
+            inventory1.items[i].number += amount;
+            return;
+        }
     }
-    return 0;
-} 
+    inventory1.putObjectsInventory(name, amount);
+}
+
+bool takeFromInventory(string name, int amount){
+    for(int i = 0; i < inventory1.items.size(); i++){
+        if(inventory1.items[i].name == name){
+            if(inventory1.items[i].number >= amount){
+                inventory1.items[i].number -= amount;
+                return true;
+            } else {
+                cout << "Not enough " << name << "! Required: " << amount << " Available: " << inventory1.items[i].number << endl;
+                return false;
+            }
+        }
+    }
+    cout << name << " not found in inventory!" << endl;
+    return false;
+}
+
+void gather(int resource){
+    time_t start = time(0);
+    int gatheredAmount = 0;
+    int gatheringExp = 0;
+    int lastSecond = -1;
+
+    string resourceName;
+    vector<oreInfo>* resourceVec;
+    switch(resource){
+        case 1: resourceName = "Ore";     resourceVec = &geders1.ore;     break;
+        case 2: resourceName = "Wood";    resourceVec = &geders1.wood;    break;
+        case 3: resourceName = "Leather"; resourceVec = &geders1.leather; break;
+        case 4: resourceName = "Wool";    resourceVec = &geders1.wool;    break;
+    }
+
+    int tier = gatheringTier - 1;
+    while(true){
+        if((int)difftime(time(0), start) >= timeEnd) break;
+        int now = (int)difftime(time(0), start);
+        if((int)fmod(now, (*resourceVec)[tier].durability * gedering1.gatheringSpeed) == 0 && now != lastSecond){
+            int yield = (int)(gedering1.gatheringYield);
+            gatheredAmount += yield;
+            addToInventory((*resourceVec)[tier].name, yield);
+            cout << resourceName << ": " << gatheredAmount << endl;
+            gatheringExp += gatheredAmount * (*resourceVec)[tier].exp;
+            cout << "Exp: " << gatheringExp << endl;
+        }
+    }
+}
+
+void refine(int resource){
+    time_t start = time(0);
+    int refinedAmount = 0;
+    int refiningExp = 0;
+    int lastSecond = -1;
+
+    string rawName;
+    string refinedName;
+    vector<oreInfo>*   rawVec;
+    vector<refinInfo>* refinedVec;
+
+    switch(resource){
+        case 1: rawName = "oreT"+to_string(refiningTier);      refinedName = "barT"+to_string(refiningTier);            rawVec = &geders1.ore;     refinedVec = &refins1.bar;            break;
+        case 2: rawName = "woodT"+to_string(refiningTier);     refinedName = "plankT"+to_string(refiningTier);          rawVec = &geders1.wood;    refinedVec = &refins1.plank;          break;
+        case 3: rawName = "leatherT"+to_string(refiningTier);  refinedName = "refinedLeatherT"+to_string(refiningTier); rawVec = &geders1.leather; refinedVec = &refins1.refinedLeather; break;
+        case 4: rawName = "woolT"+to_string(refiningTier);     refinedName = "fabricT"+to_string(refiningTier);         rawVec = &geders1.wool;    refinedVec = &refins1.fabric;         break;
+    }
+
+    int tier = refiningTier - 1;
+    while(true){
+        if((int)difftime(time(0), start) >= timeEnd) break;
+        int now = (int)difftime(time(0), start);
+        if(now != lastSecond){
+            lastSecond = now;
+            if(!takeFromInventory(rawName, 1)) break;
+            int amount = (int)(refining1.refiningEfficiency);
+            refinedAmount += amount;
+            addToInventory(refinedName, amount);
+            cout << refinedName << ": " << refinedAmount << endl;
+            refiningExp += refinedAmount * (*refinedVec)[tier].exp;
+            cout << "Exp: " << refiningExp << endl;
+        }
+    }
+}
+
+void craft(){
+    time_t start = time(0);
+    int craftedAmount = 0;
+    int lastSecond = -1;
+
+    string itemName;
+    vector<toolInfo>*  toolVec  = nullptr;
+    vector<armorInfo>* armorVec = nullptr;
+
+    switch(whatCraft){
+        case 1: itemName = "Axe";        toolVec  = &crafts1.axe;        break;
+        case 2: itemName = "Pickaxe";    toolVec  = &crafts1.pick;       break;
+        case 3: itemName = "Sword";      toolVec  = &crafts1.sword;      break;
+        case 4: itemName = "Helmet";     armorVec = &crafts1.helmet;     break;
+        case 5: itemName = "Chestplate"; armorVec = &crafts1.chestplate; break;
+        case 6: itemName = "Leggings";   armorVec = &crafts1.leggings;   break;
+        case 7: itemName = "Boots";      armorVec = &crafts1.boots;      break;
+    }
+
+    int tier = craftingTier - 1;
+    string tierStr = to_string(craftingTier);
+
+    while(true){
+        if((int)difftime(time(0), start) >= timeEnd) break;
+        int now = (int)difftime(time(0), start);
+        if(now != lastSecond){
+            lastSecond = now;
+            bool canCraft = false;
+
+            switch(whatCraft){
+                case 1: case 2:
+                    canCraft = takeFromInventory("plankT"+tierStr, 6) && takeFromInventory("barT"+tierStr, 2);
+                    break;
+                case 3:
+                    canCraft = takeFromInventory("barT"+tierStr, 20) && takeFromInventory("refinedLeatherT"+tierStr, 12);
+                    break;
+                case 4: case 5: case 6: case 7:
+                    canCraft = takeFromInventory("barT"+tierStr, 8);
+                    break;
+            }
+
+            if(canCraft){
+                craftedAmount++;
+                addToInventory(itemName+"T"+tierStr, 1);
+                cout << itemName << " T" << craftingTier << " crafted! Total: " << craftedAmount << endl;
+            } else {
+                break;
+            }
+        }
+    }
+}
 
 int main(){
 
